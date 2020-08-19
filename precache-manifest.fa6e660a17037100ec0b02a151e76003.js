9e84186c0f9a377abb0bb85ab7b821f7c7d35647
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2020-08-19 16:39:52 -0400

    Updates

---
+++ precache-manifest.fa6e660a17037100ec0b02a151e76003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f851f69e477c91a4ad53bc440ab52e",
+    "url": "/shobiz-mod4-project-frontend/index.html"
+  },
+  {
+    "revision": "74ae385a8c791741e4c5",
+    "url": "/shobiz-mod4-project-frontend/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4b518c12a164b05ba8ff",
+    "url": "/shobiz-mod4-project-frontend/static/css/main.8b98c2c4.chunk.css"
+  },
+  {
+    "revision": "74ae385a8c791741e4c5",
+    "url": "/shobiz-mod4-project-frontend/static/js/2.4cdfb4ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shobiz-mod4-project-frontend/static/js/2.4cdfb4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b518c12a164b05ba8ff",
+    "url": "/shobiz-mod4-project-frontend/static/js/main.eb8d717b.chunk.js"
+  },
+  {
+    "revision": "79d92c41b9301c820aef",
+    "url": "/shobiz-mod4-project-frontend/static/js/runtime-main.566f5eb7.js"
+  }
+]);
