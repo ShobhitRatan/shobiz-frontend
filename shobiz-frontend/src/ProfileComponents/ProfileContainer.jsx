fddfe9b6d4b317f702b50782d19d41eeb6eae296
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2021-05-10 13:58:47 +0530

    Removed reviews from profile

--- shobiz-frontend/src/ProfileComponents/ProfileContainer.jsx
+++ shobiz-frontend/src/ProfileComponents/ProfileContainer.jsx
@@ -1,11 +1,9 @@
 import React, {Component} from 'react'
-import Review from '../ReviewsComponents/Review'
 class ProfileContainer extends Component {
     render() {
         return (
             <div>
-                <h2>{this.props.user.username}&apos;s Profile</h2>
-                {this.props.user.reviews.map(review => <Review key={review.id} review={review} increaseLikes={this.props.increaseLikes} deleteReview={this.props.deleteReview}/> )} 
+                <h2>{this.props.user.username}&apos;s Profile</h2> 
             </div>
         )
     }
