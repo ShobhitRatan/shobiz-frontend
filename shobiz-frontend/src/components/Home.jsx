efa1f31a2c288674f27c8adfda2b76a0bf3f9b58
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2021-06-10 13:20:20 -0400

    done

--- shobiz-frontend/src/components/Home.jsx
+++ shobiz-frontend/src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react' 
-
+import Image from 'react-bootstrap/Image'
 const Home = () => {
     return (
         <div>
             <h1 id="header">Welcome to Shobiz! The go to site for finding movie reviews about the most popular movies. </h1>
+            <Image src="https://duetaz.org/wp-content/uploads/2018/07/Movie-Night.jpg" fluid />
         </div>
     );
 }
